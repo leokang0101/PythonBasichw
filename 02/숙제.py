07f20659ccd97da79c4b6c48063c7887593a7bb2
leokang0101 <125434766+leokang0101@users.noreply.github.com>
2023-03-08 15:14:44 +0900

    Update 숙제.py

--- 02/숙제.py
+++ 02/숙제.py
@@ -1,14 +1,15 @@
-1  #age
-#11
+1 age = 11
+print (age)
 @@@ = @@@
-2 ???
+2 print(3, 4)
 3 s1 = "변수"
 s2 = "할당"
+s + s1+ " "+ s2 + " " * 3
 s = "여의도역으로 가는 길은 어디인가요?"
 print(s[5])
 print(s[-5])
 print(s[10:12])
 print(s[13:])
 s = 오늘의 평균 기온은 {}도 입니다
-print()
-format(17, 18, 19, 20)
+print(s.format17, 18, 19, 20)
+
