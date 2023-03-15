d8c68bae83ec89b52b85b1c9226c91834bc0f44e
leokang0101 <125434766+leokang0101@users.noreply.github.com>
2023-03-15 15:16:36 +0900

    Update 숙제.py

--- 03/숙제.py
+++ 03/숙제.py
@@ -1,13 +1,20 @@
-1. input(year)
-year = @@@@@
-age = @@@@ - year
+1.
+year = int(input('당신이 턔어난 연도는?'))
+age = 2023 - year
 print(age)
 
-2. def ready_for_school():
-  print(@@@@)
-  ....
+2.def ready_for_school():
+    print("화장실에 가서 손 씻기")
+    print("옷 갈아 입기")
+    print("이발 닦기")
+    print("학교가기")
   
-@@@@()
+    
+    
+ready_for_school() 
+  
+  
+
 
 3.  def @@@@():
   print(@@@@)
@@ -18,9 +25,6 @@ go_school()
 
 ?
 
-1 = 1300
-print(exchange(20))
-print(exchange(100)
-print(exchange(365))
+
 
 ?
