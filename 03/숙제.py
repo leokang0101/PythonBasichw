b99e680a0a521dce6ed26ccb65fa04fdf73c85c9
leokang0101 <125434766+leokang0101@users.noreply.github.com>
2023-03-15 14:16:00 +0900

    Update 숙제.py

--- 03/숙제.py
+++ 03/숙제.py
@@ -1,1 +1,26 @@
+1. input(year)
+year = @@@@@
+age = @@@@ - year
+print(age)
 
+2. def ready_for_school():
+  print(@@@@)
+  ....
+  
+@@@@()
+
+3.  def @@@@():
+  print(@@@@)
+  ....
+  
+ready_for_school()
+go_school()
+
+?
+
+1 = 1300
+print(exchange(20))
+print(exchange(100)
+print(exchange(365))
+
+?
