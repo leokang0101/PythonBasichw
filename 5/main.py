136ddad89a737feb5871c710e141039ceca6318c
leokang0101 <125434766+leokang0101@users.noreply.github.com>
2023-03-29 00:26:10 +0900

    Update and rename main.py/5 to 5/main.py

---
+++ 5/main.py
@@ -0,0 +1,23 @@
+x = int(input(나이를 입력하세요 : ))
+if x == 10< =x and 20> x:
+  print("10대")
+elif x == 20<= x and 30> x:
+  print("20대")
+ elif x == 30<= x and 40> x:
+  print("30대")
+else: 
+  print("10대 미만, 40대 이상입니다.")
+ 
+??
+
+while True:
+
+count = int(input('반복할 횟수를 입력하세요: '))
+i = count
+while i<1:
+  i=i+1
+    print('Hello, world!)
+
+??
+          
+??
