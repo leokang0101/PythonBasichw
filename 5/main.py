05dd0a9afcc283895683debcc58324f79a5bcb42
leokang0101 <125434766+leokang0101@users.noreply.github.com>
2023-03-29 15:05:36 +0900

    Update main.py

--- 5/main.py
+++ 5/main.py
@@ -1,9 +1,9 @@
-x = int(input(나이를 입력하세요 : ))
-if x == 10< =x and 20> x:
+x = int(input('나이를 입력하세요 : '))
+if  10 <=x and 20>x :
   print("10대")
-elif x == 20<= x and 30> x:
+elif 20<= x and 30> x:
   print("20대")
- elif x == 30<= x and 40> x:
+elif 30 <= x and 40> x:
   print("30대")
 else: 
   print("10대 미만, 40대 이상입니다.")
