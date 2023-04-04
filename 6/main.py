02a8416ce10418be232157dcb2b607a5cae7c331
leokang0101 <125434766+leokang0101@users.noreply.github.com>
2023-04-04 21:23:17 +0900

    Create main.py

---
+++ 6/main.py
@@ -0,0 +1,29 @@
+1, 2, 3, 4, 5, 6, 7, 8, 9
+3, 9, 9, 12, 15
+5, 6, 7, 8, 9, 10
+0, 1, 2, 3, 4
+10, 8, 6, 4, 2
+number = int(input('숫자를 입력하세요.'))
+
+for i in range(1,number):
+    print(??)
+    
+for x in range(2002, 20501, 4):
+    print(x)
+    
+while True:
+    score = int(input('점수 : '))
+    
+    if score == -1:
+        print("불합격")
+    if score>=60 :
+        print('합격')
+    else :
+        print('불합격')
+        
+for i in range(10):    
+    if i == 7:
+        continue
+    print("The Number is :" , i)
+    
+???
