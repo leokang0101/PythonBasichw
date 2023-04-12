c59d19f0e5f3b9fc28abd7a4504d2baa69bf4409
leokang0101 <125434766+leokang0101@users.noreply.github.com>
2023-04-12 17:19:13 +0900

    Update main.py

--- 6/main.py
+++ 6/main.py
@@ -26,4 +26,12 @@ for i in range(10):
         continue
     print("The Number is :" , i)
     
-???
+n = int(input('숫자를 입력해 주세요.'))
+for i in range(2, n+1):
+    is_prime = True
+    for j in range(2, i, 1):
+        if i%j == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(i, end=' ')
