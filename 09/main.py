13ac875787d544a587baef507bec176e1ee7fc99
leokang0101 <125434766+leokang0101@users.noreply.github.com>
2023-04-26 00:47:49 +0900

    Create main.py

---
+++ 09/main.py
@@ -0,0 +1,17 @@
+book['모모', '어린왕자','별', '돈키호테', '마당을 나온 암탉']
+book.append('불의 날개')
+book.insert(1,'데미안')
+del book [2]
+book.remove('돈키호테')
+
+family = []
+while True:
+    name = input("가족 구성원의 이름 : ")
+    if "끝":
+        break
+        
+    family.append('강아린')
+
+print(family)
+
+#()를 사용하면 추가하거나 빼는 것을 못하기 때문이다.
