493832dda7fbf81cf36c700089254b7b01d599f2
leokang0101 <125434766+leokang0101@users.noreply.github.com>
2023-05-03 15:19:56 +0900

    Update 숙제.py

--- 10/숙제.py
+++ 10/숙제.py
@@ -3,7 +3,7 @@ for i in range(5):
     scores[i] = int(input('{}번 학생의 성적 입력: '.format(i+1)))
 print('--- 입력된 값 ---')
 for i in range(5):
-    print('{}번 학생의 성적: {}'.format(i+1,scores )
+    print('{}번 학생의 성적: {}'.format(i+1,scores[i]))
 
           
 scores = [0] * 5
